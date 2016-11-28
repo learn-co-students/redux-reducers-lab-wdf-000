21d57425a4643dbb5e5a8cf425b5454d246fa018
satub <satu.barnhill@gmail.com>
2016-11-28 13:48:08 -0500

    Done.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,16 @@
-export default function ingredientReducer() {
+export default function ingredientReducer(state=[], action) {
+
+  switch (action.type){
+    case 'ADD_INGREDIENT':
+    // console.log(action);
+    return [...state, action.payload];
+    case 'REMOVE_INGREDIENT':
+    // console.log(action);
+    let newIngredients = state;
+    newIngredients.splice(action.payload, 1);
+    return newIngredients;
+    default:
+    return state;
+  }
 
 }
