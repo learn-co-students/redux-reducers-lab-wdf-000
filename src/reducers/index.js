d90fd73b3ec2327e33cdbcafd5e38f2e5f62de44
Isuru <github email address>
2017-01-10 14:04:48 -0500

    Done.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,14 @@
-export default function ingredientReducer() {
-
+export default function ingredientReducer(state = [], action) {
+  var stat_sliced = state.slice()
+      switch(action.type){
+        case 'ADD_INGREDIENT':
+           stat_sliced.push(action.payload)
+           return stat_sliced
+        case 'REMOVE_INGREDIENT':
+          var index = stat_sliced.indexOf(action.payload);
+          stat_sliced.splice(index, 1);
+        return stat_sliced
+        default:
+          return state
+      }
 }
