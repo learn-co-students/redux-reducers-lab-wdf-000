ad0183256110f04746bafbbb915ed38b9139a484
Flatiron School <github email address>
2017-01-29 19:37:51 -0500

    done

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,21 @@
-export default function ingredientReducer() {
+
+export default function ingredientReducer(state = [], action) {
+  switch (action.type) {
+    case 'ADD_INGREDIENT':
+      return [...state, action.payload]
+    case 'REMOVE_INGREDIENT':
+          var index = 0;
+          state.forEach((ingredient, index) => {
+            if(ingredient === action.payload) {
+              index = index
+              return [...state, state.splice(index, 1)]
+              // return Object.assign([], state, state.splice(index, 1))
+            }
+            // return state
+            })
+    default:
+          return state
+          }
+
 
 }
